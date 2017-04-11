49e8a73335c753cafb6d7e7bdcc89d09dd214121
Greg Schuler <schuler@ncbi.nlm.nih.gov>
2017-04-11 12:29:35 -0400

    scripts experiments

--- scripts/version.sh
+++ scripts/version.sh
@@ -4,8 +4,8 @@ echo "========== scripts/version.sh"
 echo "version "$npm_package_version
 echo ""
 
-echo "cp build/Release/nwds.css assets/css/"
-echo "cp build/Release/_footer.html assets/html/"
-echo "git add -A assets"
+cp build/Release/nwds.css assets/css/
+cp build/Release/_footer.html assets/html/
+git add -A assets
 
 exit 0
