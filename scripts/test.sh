9ea5e71afcdbb7cafb00899d8a77a403b65aa07b
Greg Schuler <schuler@ncbi.nlm.nih.gov>
2017-04-11 10:43:44 -0400

    scripts experiments

--- scripts/test.sh
+++ scripts/test.sh
@@ -22,8 +22,9 @@ fi
 
 # TODO: run csslint on nwds.css
 
-
-cat assets/html/_footer.html | sed "s/"$npm_package_version"/_NWDS_VERSION_" > build/Release/_footer.html.tmp
+SEDEXPR="s/"$npm_package_version"/_NWDS_VERSION_/"
+echo $SEDEXPR
+cat assets/html/_footer.html | sed $SEDEXPR > build/Release/_footer.html.tmp
 
 
 
