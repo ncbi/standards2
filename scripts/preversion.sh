17b97f78d56bbe2c47b8ac3ff2464c3f03777132
Greg Schuler <schuler@ncbi.nlm.nih.gov>
2017-04-11 10:46:02 -0400

    scripts experiments

--- scripts/preversion.sh
+++ scripts/preversion.sh
@@ -8,8 +8,9 @@ echo ""
 
 # TODO: inject ncbi_nwds_ver into footer var  
 
-
-cat build/Release/_footer.html.tmp | sed "s/"_NWDS_VERSION"/$npm_package_version/" > build/Release/_footer.html
+SEDEXPR="s/_NWDS_VERSION_/"$npm_package_version"/"
+echo $SEDEXPR
+cat build/Release/_footer.html.tmp | sed $SEDEXPR > build/Release/_footer.html
 
 echo "faking an error"
 exit 1
